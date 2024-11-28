e12debf083b7f60d446b7c8a4a5e6628143552d1
Anna Sas <directive.athena@gmail.com>
2024-11-28 21:49:40 +0100

    Initial

---
+++ src/StreamerBot.AnnaSasDevLib/CPHLib.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Streamer.bot.Plugin.Interface;
+using Streamer.bot.Plugin.Interface.Enums;
+using Streamer.bot.Plugin.Interface.Model;
+using Streamer.bot.Common.Events;
+
+namespace StreamerBot.AnnaSasDevLib;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class CPHLib {
+    public static bool RunSomething(IInlineInvokeProxy cph) {
+        cph.SendMessage("Hello World, from a special place!");
+        return true;
+    }
+}
