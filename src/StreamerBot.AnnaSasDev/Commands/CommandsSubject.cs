4d25d205a8159e4d7bf3e90f0fb023cd9c045056
Anna Sas <anna.sas.dev@gmail.com>
2024-11-29 19:18:14 +0100

    Feat: Command Subject

---
+++ src/StreamerBot.AnnaSasDev/Commands/CommandsSubject.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Streamer.bot.Plugin.Interface;
+using StreamerBot.AnnaSasDev.Services.OBS;
+
+namespace StreamerBot.AnnaSasDev.Commands;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class CommandsSubject {
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public static bool CommandEntryPoint(IInlineInvokeProxy cph) {
+        // Always needed to run the various libraries that re in this assembly.
+        CphService.SetCph(cph);
+        
+        if (!CphService.TryGetArg("rawInput", out string? rawInput)) 
+            return CphService.SendFailureMessages("Could not find the rawInput argument.");
+        
+        if (!string.IsNullOrWhiteSpace(rawInput))
+            cph.SetGlobalVar(BottomPanelService.ObjectSubjectTextVarName, rawInput);
+        
+        if (!BottomPanelService.TryUpdateSubjectPanel()) 
+            return CphService.SendFailureMessages("Could not update the OBS source.");
+        
+        // Everything is nominal
+        return true;
+    }
+}
---
+++ src/StreamerBot.AnnaSasDev/Services/OBS/BottomPanelService.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Streamer.bot.Plugin.Interface;
+
+namespace StreamerBot.AnnaSasDev.Services.OBS;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class BottomPanelService {
+    public const string ObjectSubjectTextVarName = "CSharpTrackedArgument_ObsSubjectText";
+    
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public static bool TryUpdateFollowerPanel(IInlineInvokeProxy? cphEntryPoint = null) {
+        if (cphEntryPoint is not null) CphService.SetCph(cphEntryPoint);
+        if (CphService.TryGetCph(out IInlineInvokeProxy? cph) == false) return ErrorMessageService.AddErrorMessage("Could not find the CPH");
+
+        return false;
+    }
+    
+    public static bool TryUpdateSubscriberPanel(IInlineInvokeProxy? cphEntryPoint = null) {
+        if (cphEntryPoint is not null) CphService.SetCph(cphEntryPoint);
+        if (CphService.TryGetCph(out IInlineInvokeProxy? cph) == false) return ErrorMessageService.AddErrorMessage("Could not find the CPH");
+        return false;
+    }
+
+    public static bool TryUpdateSubjectPanel(IInlineInvokeProxy? cphEntryPoint = null) {
+        if (cphEntryPoint is not null) CphService.SetCph(cphEntryPoint);
+        
+        if (!CphService.TryGetCph(out IInlineInvokeProxy? cph)) return ErrorMessageService.AddErrorMessage("Could not find the CPH");
+        if (!CphService.TryGetGlobalVar(ObjectSubjectTextVarName, out string? subjectText)) return ErrorMessageService.AddErrorMessage("Could not find the subject text");
+        cph.ObsSetGdiText("Backend-Overlay-Blank", "text-subject", subjectText);
+        return true;
+    }
+}
