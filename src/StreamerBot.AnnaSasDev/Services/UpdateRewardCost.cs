9ead2dcefa5f1dbb50fd48e4dbadd8bab4268b39
Anna Sas <anna.sas.dev@gmail.com>
2024-12-20 15:15:55 +0100

    Create UpdateRewardCost.cs

---
+++ src/StreamerBot.AnnaSasDev/Services/UpdateRewardCost.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Streamer.bot.Plugin.Interface;
+
+namespace StreamerBot.AnnaSasDev.Services;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class UpdateRewardCost {
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public static bool Update(IInlineInvokeProxy cph) {
+        // Always needed to run the various libraries that re in this assembly.
+        CphService.SetCph(cph);
+        
+        if (!CphService.TryGetArg("rewardCost", out long? rewardCost)) return CphService.SendFailureMessages("Could not find the rawInput argument.");
+        if (!CphService.TryGetGlobalVar("rewardCostIncrement", out long? increment)) return CphService.SendFailureMessages("Could not find the rewardCostIncrement global variable.");
+
+        rewardCost += increment;
+        
+        cph.SetArgument("rewardCost", rewardCost);
+
+        return true;
+    }
+}
