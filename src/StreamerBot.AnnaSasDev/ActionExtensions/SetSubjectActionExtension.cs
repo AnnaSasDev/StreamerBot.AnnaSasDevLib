d76f2b9724c4db3fa342c01ebfff10989d9e3c7b
Anna Sas <anna.sas.dev@gmail.com>
2025-08-25 19:30:44 +0200

    Refactor: Introduce `SourceReferenceIds`, expand `obsData.json` scenes, and update `SetSubjectActionExtension` logic

--- src/StreamerBot.AnnaSasDev/ActionExtensions/SetSubjectActionExtension.cs
+++ src/StreamerBot.AnnaSasDev/ActionExtensions/SetSubjectActionExtension.cs
@@ -9,14 +9,13 @@ namespace StreamerBot.AnnaSasDev;
 // ---------------------------------------------------------------------------------------------------------------------
 // ReSharper disable once UnusedType.Global
 public static class SetSubjectActionExtension {
-    private const string SourceReferenceId = "panelSubjectText";
-    
     // ReSharper disable once UnusedMember.Global
     // ReSharper disable once ConvertIfStatementToReturnStatement
     public static bool ExecuteSetSubjectActionCommand(this IStreamerBotUtilities utilities) {
         if (!utilities.ChatInput.TryGetUserInput(out string? input)) throw new Exception("Could not parse user input");
         if (string.IsNullOrWhiteSpace(input)) throw new Exception("Could not find the input argument.");
-        if (!utilities.Obs.TryGetSourceReference(SourceReferenceId, out string? sceneName, out string? sourceName)) throw new Exception("Could not find the source reference.");
+        
+        if (!utilities.Obs.TryGetSourceReference(SourceReferenceIds.SetSubjectAction, out string? sceneName, out string? sourceName)) throw new Exception("Could not find the source reference.");
         return utilities.Obs.TryUpdateTextSource(sceneName, sourceName, input);
     }
 }
